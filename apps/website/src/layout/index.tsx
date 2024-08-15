374c98b8d791bf84bd4a6f3aec7d662a162552e8
momotom <a18520145493@163.com>
2024-08-16 00:38:54 +0800

    feat: LayoutContainer 布局优化

--- apps/website/src/layout/index.tsx
+++ apps/website/src/layout/index.tsx
@@ -2,28 +2,26 @@ import React from 'react';
 
 interface Props {
   children: React.ReactNode;
+  header?: React.ReactNode;
+  footer?: React.ReactNode;
 }
 
-const LayoutContainer: React.FC<Props> = ({ children }) => {
+const LayoutContainer: React.FC<Props> = ({ children, header, footer }) => {
   // 将元素分成三组
   const childrenArray = React.Children.toArray(children);
-  const header = childrenArray.find(
-    child => React.isValidElement(child) && child.type === 'header'
-  );
+  const isHeader = header
+    ? header
+    : childrenArray.find(child => React.isValidElement(child) && child.type === 'header');
   const main = childrenArray.filter(
     child => React.isValidElement(child) && child.type !== 'header' && child.type !== 'footer'
   );
-  const footer = childrenArray.filter(
-    child => React.isValidElement(child) && child.type === 'footer'
-  );
+  const isFooter = footer
+    ? footer
+    : childrenArray.filter(child => React.isValidElement(child) && child.type === 'footer');
 
   return (
     <div className="flex flex-col min-h-screen">
-      {header && (
-        <header className="">
-          <div className="container mx-auto px-4 py-4">{header}</div>
-        </header>
-      )}
+      {isHeader && <div className="container mx-auto px-4 py-4">{isHeader}</div>}
 
       {main && (
         <main className="flex-grow ">
@@ -31,11 +29,7 @@ const LayoutContainer: React.FC<Props> = ({ children }) => {
         </main>
       )}
 
-      {footer && (
-        <footer className="">
-          <div className="container mx-auto px-4 py-6">{footer}</div>
-        </footer>
-      )}
+      {isFooter && <div className="container mx-auto px-4 py-6">{isFooter}</div>}
     </div>
   );
 };
