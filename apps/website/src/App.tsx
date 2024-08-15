da038f1f7cb9e39a8555e65c354e8679be2b8ef4
momotom <a18520145493@163.com>
2024-08-16 00:31:11 +0800

    feat: 切换主题

--- apps/website/src/App.tsx
+++ apps/website/src/App.tsx
@@ -1,11 +1,43 @@
+import { Button } from '@headlessui/react';
 import './App.css';
 import LayoutContainer from './layout';
+import { ThemeManager } from '@theme-smooth/core';
 
 function App() {
+  const themeManager = new ThemeManager({
+    transitionDuration: 800,
+    transitionEffect: 'circle',
+  });
   return (
     <LayoutContainer>
-      <header>
+      <header className="flex w-full items-center justify-between">
         <h1 className="text-2xl font-bold">我的应用</h1>
+        <div className=" space-x-2">
+          <Button
+            className="border p-2 rounded-md"
+            onClick={() => {
+              themeManager.setTheme('light');
+            }}
+          >
+            Light Theme
+          </Button>
+          <Button
+            className="border p-2 rounded-md"
+            onClick={() => {
+              themeManager.setTheme('dark');
+            }}
+          >
+            Dark Theme
+          </Button>
+          <Button
+            className="border p-2 rounded-md"
+            onClick={() => {
+              themeManager.toggleTheme();
+            }}
+          >
+            toggle theme
+          </Button>
+        </div>
       </header>
 
       <section>
@@ -16,7 +48,6 @@ function App() {
       <footer>
         <p>&copy; 2024 我的公司。保留所有权利。</p>
       </footer>
-
     </LayoutContainer>
   );
 }
