324a508531fafa854e021171ed36ec0c5baa6a3b
momotom <a18520145493@163.com>
2024-08-17 11:00:19 +0800

    feat: button

--- apps/website/src/App.tsx
+++ apps/website/src/App.tsx
@@ -3,9 +3,9 @@ import LayoutContainer from './layout';
 import Logo from './components/common/Logo';
 import { ThemeProvider } from './components/theme/ThemeContextType';
 import ThemeControls from './components/ThemeControls';
+import JellyButton from './components/common/JellyButton';
 
 function App() {
-
   return (
     <ThemeProvider>
       <LayoutContainer>
@@ -16,9 +16,14 @@ function App() {
           <ThemeControls />
         </header>
 
-        <section className='w-full flex flex-col items-center justify-center space-y-4 xl:py-28 lg:py-28 md:py-28 sm:py-16 sx:py-8'>
-          <h2 className="sm:text-3xl dark:text-[#fff] font-bold sx:text-xl">Out-of-the-box loading icons</h2>
-          <p className='dark:text-[#fff] '>这里是主要内容区域。</p>
+        <section className="w-full flex flex-col items-center justify-center space-y-4 xl:py-28 lg:py-28 md:py-28 sm:py-16 sx:py-8">
+          <h2 className="sm:text-3xl dark:text-[#fff] font-bold sx:text-xl">
+            Out-of-the-box loading icons
+          </h2>
+          <div className='space-x-4'>
+            <JellyButton color='sky' className='dark:text-white'>Get Started</JellyButton>
+            <JellyButton color='gray' className='dark:text-whit'>Download all</JellyButton>
+          </div>
         </section>
 
         <footer className="container mx-auto px-4 py-6">
