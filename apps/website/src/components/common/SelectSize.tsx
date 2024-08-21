1c589d94205c11edbb7d70338dc9f65b90566f6c
momotom <a18520145493@163.com>
2024-08-21 22:45:03 +0800

    feat: size-select组件

---
+++ apps/website/src/components/common/SelectSize.tsx
@@ -0,0 +1,63 @@
+import { Fragment, useState } from 'react';
+import { Listbox, Transition, ListboxOptions, ListboxButton, ListboxOption } from '@headlessui/react';
+import { ChevronsUpDown, Check } from 'lucide-react';
+
+const sizes = [
+  { name: 'XS', value: 'xs' },
+  { name: 'S', value: 's' },
+  { name: 'M', value: 'm' },
+  { name: 'L', value: 'l' },
+  { name: 'XL', value: 'xl' },
+];
+
+function SelectSize() {
+  const [selectedSize, setSelectedSize] = useState(sizes[2]);
+
+  return (
+    <Listbox value={selectedSize} onChange={setSelectedSize}>
+      <div className="relative mt-1">
+        <ListboxButton className="relative w-full cursor-default rounded-lg bg-white py-2 pl-3 pr-10 text-left shadow-md focus:outline-none focus-visible:border-indigo-500 focus-visible:ring-2 focus-visible:ring-white focus-visible:ring-opacity-75 focus-visible:ring-offset-2 focus-visible:ring-offset-orange-300 sm:text-sm">
+          <span className="block truncate">{selectedSize.name}</span>
+          <span className="pointer-events-none absolute inset-y-0 right-0 flex items-center pr-2">
+            <ChevronsUpDown className="h-5 w-5 text-gray-400" aria-hidden="true" />
+          </span>
+        </ListboxButton>
+        <Transition
+          as={Fragment}
+          leave="transition ease-in duration-100"
+          leaveFrom="opacity-100"
+          leaveTo="opacity-0"
+        >
+          <ListboxOptions className="absolute mt-1 max-h-60 w-full overflow-auto rounded-md bg-white py-1 text-base shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none sm:text-sm">
+            {sizes.map((size) => (
+              <ListboxOption
+                key={size.value}
+                className={({ active }) =>
+                  `relative cursor-default select-none py-2 pl-10 pr-4 ${
+                    active ? 'bg-amber-100 text-amber-900' : 'text-gray-900'
+                  }`
+                }
+                value={size}
+              >
+                {({ selected }) => (
+                  <>
+                    <span className={`block truncate ${selected ? 'font-medium' : 'font-normal'}`}>
+                      {size.name}
+                    </span>
+                    {selected ? (
+                      <span className="absolute inset-y-0 left-0 flex items-center pl-3 text-amber-600">
+                        <Check className="h-5 w-5" aria-hidden="true" />
+                      </span>
+                    ) : null}
+                  </>
+                )}
+              </ListboxOption>
+            ))}
+          </ListboxOptions>
+        </Transition>
+      </div>
+    </Listbox>
+  );
+}
+
+export default SelectSize;
